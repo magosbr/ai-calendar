7b11dd825263df3e28a010424e9c77d700ac10e8
Gustavo Brancaglione <gustavo.brancaglione@gmail.com>
2024-07-08 10:26:29 -0300

    Temp trying to deploy

--- src/core/implementations/google_calendar/GoogleCalendarRepository.ts
+++ src/core/implementations/google_calendar/GoogleCalendarRepository.ts
@@ -1,13 +1,24 @@
 import { google, calendar_v3 } from 'googleapis';
 import { ICalendarRepository } from '../../repositories/ICalendarRepository';
 import { Event } from '../../models/Event';
-import fs from 'fs';
-import path from 'path';
 import "dotenv/config";
 
-const CREDENTIALS_PATH = path.join(__dirname, '../../../../credentials.json');
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-const CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID || '';
+const CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID;
+
+const CREDENTIALS = {
+    "type": "service_account",
+    "project_id": "ai-calendar-161920",
+    "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
+    "private_key": process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    "client_email": "ai-calendar@ai-calendar-161920.iam.gserviceaccount.com",
+    "client_id": process.env.GOOGLE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ai-calendar%40ai-calendar-161920.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+};
 
 export class GoogleCalendarRepository implements ICalendarRepository {
     private auth: any;
@@ -19,9 +30,8 @@ export class GoogleCalendarRepository implements ICalendarRepository {
     }
 
     private initializeAuth() {
-        const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, 'utf-8'));
         return new google.auth.GoogleAuth({
-            credentials,
+            credentials: CREDENTIALS,
             scopes: SCOPES,
         });
     }
