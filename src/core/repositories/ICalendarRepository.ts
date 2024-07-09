431cfec652e96c6bcf206ebc8565c221cd0911e7
gbrancaglione <45099000+gbrancaglione@users.noreply.github.com>
2024-07-09 16:30:48 -0300

    Fix heroku deploy and js compilation  (#10)
    Fix js compilation

--- src/core/repositories/ICalendarRepository.ts
+++ src/core/repositories/ICalendarRepository.ts
@@ -3,4 +3,6 @@ import { Event } from '../models/Event';
 export interface ICalendarRepository {
     createEvent(event: Event): Promise<string>;
     getEvents(startDate: string, endDate: string): Promise<Event[]>;
+    updateEvent(eventId: string, event: Event): Promise<string>;
+    deleteEvent(eventId: string): Promise<void>;
 }
--- src/web/server.ts
+++ src/web/server.ts
@@ -1,17 +1,19 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { createEvent, getEvents } from './api/controllers/CalendarController';
 import "dotenv/config";
+import bodyParser from 'body-parser';
+import { createEvent, getEvents, updateEvent, deleteEvent } from './api/controllers/CalendarController';
 import { validateRequest } from './api/middlewares/ValidationMiddleware';
-import { eventSchema } from './api/schemas/EventSchema';
+import { EventSchema } from './api/schemas/EventSchema';
 
 const app = express();
-const PORT = process.env.APP_PORT;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/create', validateRequest(eventSchema), createEvent);
+app.post('/create', validateRequest(EventSchema), createEvent);
 app.get('/events', getEvents);
+app.put('/events/:eventId', validateRequest(EventSchema), updateEvent);
+app.delete('/events/:eventId', deleteEvent);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
