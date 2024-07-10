4ccd4d46755d202b9d9f4309310ddda6eddac0e3
Gustavo Brancaglione <gustavo.brancaglione@gmail.com>
2024-07-10 00:17:12 -0300

    Add landing page

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
+import path from 'path';
 import { createEvent, getEvents, updateEvent, deleteEvent } from './api/controllers/CalendarController';
 import { validateRequest } from './api/middlewares/ValidationMiddleware';
 import { EventSchema } from './api/schemas/EventSchema';
@@ -10,10 +11,16 @@ const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, '../..', 'public')));
+
 app.post('/create', validateRequest(EventSchema), createEvent);
 app.get('/events', getEvents);
 app.put('/events/:eventId', validateRequest(EventSchema), updateEvent);
 app.delete('/events/:eventId', deleteEvent);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../..', 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
