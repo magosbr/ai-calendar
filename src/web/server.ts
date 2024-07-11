ae0b3bfc346b6919df908b07b98ce1f91d35e87a
Gustavo Brancaglione <gustavo.brancaglione@gmail.com>
2024-07-10 22:12:05 -0300

    Testing

--- src/web/server.ts
+++ src/web/server.ts
@@ -18,6 +18,22 @@ app.get('/events', getEvents);
 app.put('/events/:eventId', validateRequest(EventSchema), updateEvent);
 app.delete('/events/:eventId', deleteEvent);
 
+app.get("/webhook", (req, res) => {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    // check the mode and token sent are correct
+    if (mode === "subscribe" && token === "hello") {
+        // respond with 200 OK and challenge token from the request
+        res.status(200).send(challenge);
+        console.log("Webhook verified successfully!");
+    } else {
+        // respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+    }
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../..', 'public', 'index.html'));
 });
